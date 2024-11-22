1a7f908afbd661ae924963fad0bfd3cf5b6376e2
Liagiba03 <143125742+Liagiba03@users.noreply.github.com>
2024-11-21 22:10:46 -0600

    update menu

--- menu.py
+++ menu.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import ttk
 import subprocess
 import os
 
@@ -11,43 +12,173 @@ def run_script(script_name):
     except Exception as e:
         print(f"Error al ejecutar {script_name}: {e}")
 
+def crear_label(texto, frame):
+    label = tk.Label(frame, text=texto, font=("Arial", 18, "bold"), fg="#cdd4ea", bg="#0f1440")
+    return label
+
+def separator(frame):
+    separator_canvas = tk.Canvas(frame, height=5, bg="#bc6fc7", highlightthickness=0)
+    separator_canvas.pack(fill="x", padx=10, pady=(0, 10))
+    return separator_canvas
+
+def create_frame_horzontal(frame1):
+    frame = tk.Frame(frame1, bg="#0f1440")
+    return frame
+
+def create_button(frame, texto, script):
+    button = tk.Button(
+        frame,
+        text=texto,
+        command=lambda: run_script(script),
+        font=("Sans-Serif", 12, "bold"),
+        fg="#ffffff",  # Color del texto
+        bg="#282e61",  # Color de fondo del botón
+        activebackground="#636cb4",  # Color de fondo cuando el botón está activo
+        highlightthickness=0,  # Grosor del contorno
+        highlightbackground="#282e61",  # Color del contorno (mismo que el fondo del botón)
+        highlightcolor="#282e61"  # Color del contorno al enfocar el botón
+    )
+    return button
+
+
 # Crear la ventana principal
 root = tk.Tk()
 root.title("Sistema de Monitoreo")
-root.configure(bg="#34495e")  # Fondo con un tono más oscuro
+root.configure(bg="#0f1440")  # Fondo con un tono más oscuro
 
 # Frame para centrar los botones
-frame = tk.Frame(root, bg="#34495e")
-frame.pack(expand=True, padx=20, pady=20)
+frame_principal = tk.Frame(root, bg="#0f1440")
+frame_principal.pack(fill="both", expand=True, padx=10, pady=10)
+frame_principal.columnconfigure(0, weight=1)
+frame_principal.columnconfigure(1, weight=1)
 
-# Etiqueta de título
-title_label = tk.Label(frame, text="SISTEMA DE MONITOREO", font=("Helvetica", 18, "bold"), fg="#ecf0f1", bg="#34495e")
-title_label.pack(pady=(0, 30))
+# Etiqueta de título (centrada en el frame_principal)
+title_label = tk.Label(frame_principal, text="SISTEMA DE MONITOREO", font=("Segoe UI", 25, "bold"), fg="#ecf0f1", bg="#0f1440")
+title_label.grid(row=0, column=0, columnspan=2, pady=(0, 30), sticky="nsew")  # Ocupa ambas columnas
+
+frame = create_frame_horzontal(frame_principal)
+frame.grid(row=1, column = 0, padx=10, pady=10,  sticky="nsew")
+
+frame2 = create_frame_horzontal(frame_principal)
+frame2.grid(row=1, column = 1 , padx=10, pady=10,  sticky="nsew")
+
+#--------------------- MONITOREO--------------------------
+# Subtítulo y separador para la sección de monitoreo
+subtitle_label = crear_label("Monitorear", frame)
+subtitle_label.pack(pady=(10, 5))  # Espaciado superior e inferior
+
+# Separador
+separator(frame)
+
+# Frame para botones de pantallas
+frame_pantalla = create_frame_horzontal(frame)
+frame_pantalla.pack(pady=(10,30))
 
 # Botones mejorados para ejecutar diferentes scripts
-button1 = tk.Button(frame, text="Compartir Pantalla", command=lambda: run_script("servidor.py"),
-                    font=("Helvetica", 12, "bold"), fg="#ffffff", bg="#e74c3c", activebackground="#c0392b")
-button1.pack(pady=10, ipadx=20, ipady=10)
+button1 = create_button(frame_pantalla,"Compartir Pantalla","servidor.py")
+button1.grid(row=0, column=0, padx=10, ipadx=20, ipady=10)
 
-button2 = tk.Button(frame, text="Ver Pantalla Externa", command=lambda: run_script("cliente.py"),
-                    font=("Helvetica", 12, "bold"), fg="#ffffff", bg="#e74c3c", activebackground="#c0392b")
-button2.pack(pady=10, ipadx=20, ipady=10)
+button2 = create_button(frame_pantalla,"Ver Pantalla Externa","cliente.py")
+button2.grid(row=0, column=1, padx=10, ipadx=20, ipady=10)
 
-button3 = tk.Button(frame, text="Iniciar Sala de Chats", command=lambda: run_script("servidor_mensajes.py"),
-                    font=("Helvetica", 12, "bold"), fg="#ffffff", bg="#e74c3c", activebackground="#c0392b")
-button3.pack(pady=10, ipadx=20, ipady=10)
+#---------------------CHAT--------------------------
+# Subtítulo y separador para la sección de monitoreo
+subtitle_label= crear_label("Chatear", frame)
+subtitle_label.pack(pady=(10, 5))  # Espaciado superior e inferior
 
-button4 = tk.Button(frame, text="Conectar a Sala de Chat", command=lambda: run_script("cliente_mensajes.py"),
-                    font=("Helvetica", 12, "bold"), fg="#ffffff", bg="#e74c3c", activebackground="#c0392b")
-button4.pack(pady=10, ipadx=20, ipady=10)
+# Separador
+separator(frame)
 
-button5 = tk.Button(frame, text="Enviar y recibir archivos", command=lambda: run_script("archivos.py"),
-                    font=("Helvetica", 12, "bold"), fg="#ffffff", bg="#e74c3c", activebackground="#c0392b")
+# Frame para botones de pantallas
+frame_chat = create_frame_horzontal(frame)
+frame_chat.pack(pady=(10,30))
+
+button3 = create_button(frame_chat, "Iniciar Sala de Chat", "servidor_mensajes.py")
+button3.grid(row=0, column=0, padx=10, ipadx=20, ipady=10)
+
+button4 = create_button(frame_chat, "Conectar a Sala de Chat", "cliente_mensajes.py")
+button4.grid(row=0, column=1, padx=10, ipadx=20, ipady=10)
+
+#---------------------ENVIAR / RECIBIR ARCHIVOS--------------------------
+# Subtítulo y separador para la sección de recibir archivos
+subtitle_label= crear_label("Recibir / Enviar archivos", frame)
+subtitle_label.pack(pady=(10, 5))  # Espaciado superior e inferior
+
+# Separador
+separator(frame)
+
+button5 = create_button(frame,"Enviar y recibir archivos", "archivos.py")
 button5.pack(pady=10, ipadx=20, ipady=10)
 
+
+#---------------------APAGAR COMPUTADOR--------------------------
+# Subtítulo y separador para la sección de apagar
+subtitle_label= crear_label("Apagar computador remoto", frame2)
+subtitle_label.pack(pady=(10, 5))  # Espaciado superior e inferior
+
+# Separador
+separator(frame2)
+
+button6 = create_button(frame2, "Apagar pc remota", "apagarUbuntu.py")
+button6.pack(pady=10, ipadx=20, ipady=10)
+
+#--------------------DENEGAR/PERMITIR PING-------------------------
+# Subtítulo y separador para la sección de ping
+subtitle_label= crear_label("Denegar / Permitir Ping ", frame2)
+subtitle_label.pack(pady=(10, 5))  # Espaciado superior e inferior
+
+# Separador
+separator(frame2)
+
+button6 = create_button(frame2, "Ping remoto", "denegarping.py")
+button6.pack(pady=10, ipadx=20, ipady=10)
+
+
+#-----------------BLOQUEA/DESBLOQUEAR MOUSE/ TECLADO-----------------------
+# Subtítulo y separador para la sección de acceso a páginas
+subtitle_label= crear_label("Controlar mouse / teclado ", frame2)
+subtitle_label.pack(pady=(10, 5))  # Espaciado superior e inferior
+
+# Separador
+separator(frame2)
+
+#button8 = create_button(frame2, "Ping remoto", "denegarping.py")
+#button8.pack(pady=10, ipadx=20, ipady=10)
+
+#-------------------DENEGAR ACCESO A PÁGINAS--------------------------
+# Subtítulo y separador para la sección de acceso a páginas
+subtitle_label= crear_label("Control de acceso a páginas web ", frame2)
+subtitle_label.pack(pady=(10, 5))  # Espaciado superior e inferior
+
+# Separador
+separator(frame2)
+
+#button7 = create_button(frame2, "Ping remoto", "denegarping.py")
+#button7.pack(pady=10, ipadx=20, ipady=10)
+
+
 # Ajustar el tamaño de la ventana
-root.geometry("500x450")
-root.resizable(False, False)
+
+# Hacer que la ventana ajuste su tamaño según el contenido
+root.update_idletasks()  # Asegura que la ventana haya calculado su tamaño
+
+# Obtener las dimensiones de la pantalla
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+
+# Obtener las dimensiones de la ventana (después de que se haya ajustado al contenido)
+window_width = root.winfo_width()
+window_height = root.winfo_height()
+
+# Calcular las coordenadas para centrar la ventana
+x = (screen_width // 2) - (window_width // 2)
+y = (screen_height // 2) - (window_height // 2)
+
+# Establecer la geometría de la ventana con las coordenadas calculadas
+root.geometry(f"+{x}+{y}")
+
+#root.geometry("500x500")
+root.resizable(True, True)
 
 # Iniciar el bucle principal de la interfaz
 root.mainloop()
